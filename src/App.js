4dc8b40f014fcfbeae56ad09eed4da3540563324
Boris <42873497+aboriskaa@users.noreply.github.com>
2022-09-13 23:02:37 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+import CustomTabsHook from "./AddAndDeleteTab";
+export default function App() {
+  return (
+    <div className="App">
+      <CustomTabsHook />
+    </div>
+  );
+}
